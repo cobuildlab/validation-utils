43acba48c3e8a6ad441ae5310e56ae0082a4375e
GerardoAGL96 <gerardoagl96@gmail.com>
2020-06-26 10:16:17 -0400

    Email, number, integer and date validations added

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -1,5 +1,11 @@
-import {isNullOrUndefined, isValidString} from '../index';
-/* global test, expect */
+import {
+  isNullOrUndefined, 
+  isValidString,
+  isValidEmail,
+  isValidNumber,
+  isValidInteger,
+  isValidDate
+} from '../index';
 
 test('isNullOrUndefined:', () => {
   expect(isNullOrUndefined(null)).toBe(true);
@@ -26,6 +32,51 @@ test('isValidString:', () => {
 
   expect(isValidString("Some String")).toBe(true);
   expect(isValidString("", true)).toBe(true);
-
-
 });
+
+test('isValidEmail', () => {
+  expect(isValidEmail('')).toBe(false);
+  expect(isValidEmail('Some string')).toBe(false);
+  expect(isValidEmail('jhondoe@com')).toBe(false);
+  expect(isValidEmail('@gmail.com')).toBe(false);
+
+  expect(isValidEmail('jhondoe@gmail.com')).toBe(true);
+});
+
+test('isValidNumber', () => {
+  expect(isValidNumber('')).toBe(false);
+  expect(isValidNumber('Some string')).toBe(false);
+  expect(isValidNumber('123a424')).toBe(false);
+  expect(isValidNumber('123a424', true)).toBe(false);
+  expect(isValidNumber('123a424', true, true)).toBe(false);
+  expect(isValidNumber('123a424', false, true)).toBe(false);
+  expect(isValidNumber('1.23a424', false, true)).toBe(false);
+  expect(isValidNumber('123-424', false, true)).toBe(false);
+  expect(isValidNumber('0')).toBe(false);
+  
+  expect(isValidNumber('0', true)).toBe(true);
+  expect(isValidNumber('-1', true, true)).toBe(true);
+  expect(isValidNumber('201')).toBe(true);
+  expect(isValidNumber('3.14159')).toBe(true);
+});
+
+test('isValidInteger', () => {
+  expect(isValidInteger(3.14159)).toBe(false);
+  expect(isValidInteger(0)).toBe(false);
+  expect(isValidInteger(-1)).toBe(false);
+  
+  expect(isValidInteger(0, true)).toBe(true);
+  expect(isValidInteger(-1, false, true)).toBe(true);
+  expect(isValidInteger(10)).toBe(true);
+});
+
+test('isValidDate', () => {
+  expect(isValidDate('')).toBe(false);
+  expect(isValidDate('Some string')).toBe(false);
+  expect(isValidDate('04939-343.643')).toBe(false);
+  expect(isValidDate('2020-12-31', 'ddd MMM DD YYY HH:mm:ss')).toBe(false);
+  
+  expect(isValidDate('2020-06-26', 'YYYY-MM-DD')).toBe(true);
+  expect(isValidDate('Mar 25 2015', 'MMM DD YYYY')).toBe(true);
+  expect(isValidDate('Tue Mar 24 2015 19:30:00', 'ddd MMM DD YYY HH:mm:ss')).toBe(true);
+})
